21bc15134e5f5aea1860c315dbbca31aa68c64f2
chenqilin70 <kylinchen85@foxmail.com>
2024-10-08 11:18:35 +0800

    update

---
+++ c2n-contracts/scripts/deployment/deploy_refresh_config.js
@@ -0,0 +1,13 @@
+let timestamp = Math.floor(Date.now() / 1000);  
+console.log("timestamp:",timestamp);
+
+registrationStartAt = timestamp + 300
+console.log("registrationStartAt:",registrationStartAt);
+
+TGE = timestamp + 720
+console.log("TGE:",TGE);
+
+unlockingTimes =  timestamp + 840
+console.log("unlockingTimes:",unlockingTimes);
+
+
